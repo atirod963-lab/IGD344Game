33c976fe1de1f99959b2295a95908809dad2494a
atirod963-lab <atirod.963@gmail.com>
2026-04-20 14:08:05 +0700

    เพิ่มBGM

---
+++ Assets/Script/sound/BGMPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+
+public class BGMPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        if (SoundManager.Instance != null) SoundManager.Instance.PlayBGM("BGM");
+
+    }
+
+}
